5826842c5315c013a44336466131653b47550030
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 11:50:39 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -3,7 +3,7 @@ import streamlit as st
 import base64
 from st_aggrid import AgGrid, GridOptionsBuilder
 import os
-import gitpush
+#import gitpush
 
 # Initial configurations
 ####################################
@@ -11,3 +11,6 @@ st.set_page_config(page_title = 'RevMaster', page_icon = ':books:', layout = 'wi
 
 config_files = os.listdir('configs')
 st.text(config_files)
+if 'initial_config.txt' not in config_files:
+  st.header('No configuration found.')
+  st.subheader('Let\'s set up a new project')
