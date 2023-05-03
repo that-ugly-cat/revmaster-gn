186a4d425a16396c10679003191748d36060b46e
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 16:16:26 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -85,4 +85,13 @@ else:
   lines = test_read.readlines()
   for line in lines:
     st.write(line)
+  from google.cloud import firestore
+  # Authenticate to Firestore with the JSON account key.
+  db = firestore.Client.from_service_account_json("firestore-key.json")
+  # Create a reference to the Google post.
+  doc_ref = db.collection("papers").document("a paper")
+  doc = doc_ref.get()
+  # Let's see what we got!
+  st.write("The id is: ", doc.id)
+  st.write("The contents are: ", doc.to_dict())(
 
