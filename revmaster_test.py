d2bbf30877fef18e8cef1b6bf9f81366b9df8b00
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 16:33:05 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -25,13 +25,6 @@ if 'initial_config.py' not in config_files:
       criteria = st.text_area('Assessment criteria', 'one\nper\nline')
       st.divider()
       ###
-      st.subheader('Database configuration')
-      db_host = st.text_input('host', '')
-      db_user = st.text_input('user', '')
-      db_password = st.text_input('password', '')
-      db_database = st.text_input('database', '')
-      ###
-      st.divider()
       st.subheader('Upload the CSV file containing your papers to be assessed.')
       st.text('The file should contain at least the following columns to function properly: Key (unique identifier), Publication Year, Author, Title.')
       st.text('The code is tested and optimized for Zotero collection exports.')
@@ -42,7 +35,7 @@ if 'initial_config.py' not in config_files:
       ###
       save_1 = st.form_submit_button("Save")
       if save_1:
-        with open('initial_config.py', 'w') as f:
+        '''with open('initial_config.py', 'w') as f:
           l1 = 'project_title = \'' + project_title + '\'\n'
           l2 = 'project_description = \'' + project_description + '\'\n'
           criteria = criteria.split('\n')
@@ -54,30 +47,19 @@ if 'initial_config.py' not in config_files:
             if criterion == last_item:
               critlist = critlist + '\'' + criterion + '\']\n'
           l3 = critlist
-          l4 = 'db_host = \'' + db_host + '\'\n'
-          l5 = 'db_user = \'' + db_user + '\'\n'
-          l6 = 'db_password = \'' + db_password + '\'\n'
-          l7 = 'db_database = \'' + db_database + '\'\n'
-          f.writelines([l1, l2, l3, l4, l5, l6, l7])
+          f.writelines([l1, l2, l3])'''
         ###
-        mydb = mysql.connector.connect(
-          host = db_host,
-          user = db_user,
-          password = db_password,
-          database=db_database)
-        mycursor = mydb.cursor()
-        mycursor.execute("SHOW TABLES")
-        for x in mycursor:
-          st.write(x)
+        out = papers_df.to_dict()
+        firebase.post("/testupdate", out)
         ###
-        test_read = open('initial_config.py', 'r')
+        '''test_read = open('initial_config.py', 'r')
         lines = test_read.readlines()
         for line in lines:
           st.write(line)
         user = st.secrets['github_user']
         token = st.secrets['github_token']
         repo = st.secrets['github_repo']
-        gitpush.git_save('initial_config.py', user, token, repo)
+        gitpush.git_save('initial_config.py', user, token, repo)'''
 
 else:
   st.text('...')
