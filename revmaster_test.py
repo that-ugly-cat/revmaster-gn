acf81d7851f7b29d9f08160d88f827190108e91a
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 11:58:53 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -11,6 +11,15 @@ st.set_page_config(page_title = 'RevMaster', page_icon = ':books:', layout = 'wi
 
 config_files = os.listdir('configs')
 st.text(config_files)
-if 'initial_config.txt' not in config_files:
+if 'initial_config.py' not in config_files:
   st.header('No configuration found.')
   st.subheader('Let\'s set up a new project')
+  with st.form("my_form"):
+    project_title = st.text_input('Project title', '...')
+    project_description = st.text_area('Text to analyze', '...')
+    save_1 = st.form_submit_button("Save")
+  if save_1:
+    st.write(project_title, project_description)
+
+else:
+  st.text('...')
