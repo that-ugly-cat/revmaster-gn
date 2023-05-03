b8debd997118fc214eb8137266e92a23fba982b5
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 12:05:21 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -14,12 +14,19 @@ st.text(config_files)
 if 'initial_config.py' not in config_files:
   st.header('No configuration found.')
   st.subheader('Let\'s set up a new project')
-  with st.form("my_form"):
-    project_title = st.text_input('Project title', '...')
-    project_description = st.text_area('Text to analyze', '...')
-    save_1 = st.form_submit_button("Save")
-  if save_1:
-    st.write(project_title, project_description)
+  with st.expander('Assessment criteria', expanded = True):
+    with st.form("form_1"):
+      project_title = st.text_input('Project title', '...')
+      project_description = st.text_area('Text to analyze', '...')
+      save_1 = st.form_submit_button("Save")
+    if save_1:
+      st.write(project_title, project_description)
+  with st.expander('Assessment criteria', expanded = True):
+    with st.form("form_2"):
+      criteria = st.text_area('Assessment criteria', 'one\nper\nline')
+      save_2 = st.form_submit_button("Save")
+    if save_2:
+      st.write(criteria)    
 
 else:
   st.text('...')
