a2293a60806d0774edc5995c6542833a01f3f400
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 11:47:48 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -4,3 +4,10 @@ import base64
 from st_aggrid import AgGrid, GridOptionsBuilder
 import os
 import gitpush
+
+# Initial configurations
+####################################
+st.set_page_config(page_title = 'RevMaster', page_icon = ':books:', layout = 'wide')
+
+config_files = os.listdir('configs')
+st.text(config_files)
