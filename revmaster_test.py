3d33b6a6a8069984cf9f347d46e5bcbe105ba131
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-03 13:02:24 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -18,17 +18,21 @@ if 'initial_config.py' not in config_files:
     with st.form("form_1"):
       project_title = st.text_input('Project title', '...')
       project_description = st.text_area('Text to analyze', '...')
+      criteria = st.text_area('Assessment criteria', 'one\nper\nline')
       save_1 = st.form_submit_button("Save")
     if save_1:
       st.write(project_title, project_description)
-      with st.expander('Assessment criteria', expanded = True):
-        with st.form("form_2"):
-          criteria = st.text_area('Assessment criteria', 'one\nper\nline')
-          save_2 = st.form_submit_button("Save")
-        if save_2:
-          st.write(criteria)
-          with open('configs/initial_config.py', w) as f:
-            f.write('project_title = ' + project_title)
+      with open('configs/initial_config.py', w) as f:
+        l1 = 'project_title = ' + project_title + '\n'
+        l2 = 'project_description = ' + project_description + '\n'
+        l3 = 'criteria = ' + criteria + '\n'
+      f.writelines([l1, l2, l3])
+      test_read = open('configs/initial_config.py', 'r')
+      lines = test_read.readlines()
+      for line in lines:
+        st.write(line)
+      
+      
 
 else:
   st.text('...')
