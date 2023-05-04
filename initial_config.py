23ff83618b487bb300b423c541517682bd04bcd5
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-04 17:34:44 +0200

    Update initial_config.py

--- initial_config.py
+++ initial_config.py
@@ -1,4 +1,4 @@
-project_title = 'test project'
-project_description = 'my project'
-criteria = ['one', 'per', 'line', 'and more', 'if needed']
+project_title = 'Ethical considerations on social listening and infodemic management'
+project_description = 'See details [here](https://osf.io/28d73/wiki/home/)'
+criteria = ['Infodemic: Characterization', 'Infodemic management : Characterization', '	Ethical Issues in Infodemics', '	Ethical Issues in Infodemic management', 'Aims of Infodemic management', 'Conclusions and recommendations']
 firestore_collection = 'papers_WHO'
