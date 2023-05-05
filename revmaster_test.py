b1ae541de86b5d478a2dd9879252660500a23bf5
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-05 11:02:22 +0200

    Update revmaster_test.py

--- revmaster_test.py
+++ revmaster_test.py
@@ -56,6 +56,7 @@ if 'initial_config.py' not in config_files:
         toml_config = toml.dumps(config)
         st.subheader('Copy this in your streamlit secrets:')
         toml_config_decode = toml_config.replace('textkey = [ ', '').replace(',]', '')
+        toml_config_decode = toml_config_decode.split(', ')
         st.write(toml_config_decode)
         #gitpush.git_save(output_file, git_user, git_token, git_repo)
         #key_dict = json.loads(config)
