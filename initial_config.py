e3217e007d3a891c826f3226261c975c80eeae13
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-05 09:17:29 +0200

    Delete initial_config.py

--- initial_config.py
+++
@@ -1,4 +0,0 @@
-project_title = 'Ethical considerations on social listening and infodemic management'
-project_description = 'See details [here](https://osf.io/28d73/wiki/home/)'
-criteria = ['Infodemic: Characterization', 'Infodemic management : Characterization', 'Ethical Issues in Infodemics', 'Ethical Issues in Infodemic management', 'Aims of Infodemic management', 'Conclusions and recommendations']
-firestore_collection = 'papers_WHO'
