e47fade861221ac5ed2ff03ca7ae7111f6d2eae1
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-05 12:17:10 +0200

    Data saved from app

---
+++ initial_config.py
@@ -0,0 +1,4 @@
+project_title = 'Ethical considerations on social listening and infodemic management'
+project_description = 'Scoping review. info and details [here](https://osf.io/28d73/wiki/home/)'
+criteria = ['Infodemic: Characterization', 'Infodemic management : Characterization', 'Ethical Issues in Infodemics', 'Ethical Issues in Infodemic management', 'Aims of Infodemic management', 'Conclusions and recommendations']
+firestore_collection = 'papers_who_stable'
