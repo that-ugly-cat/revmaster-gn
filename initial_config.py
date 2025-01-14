4ee694a5de7d3e983a50bfee77a0b33bb56ac827
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2025-01-14 11:09:49 +0100

    Data saved from app

---
+++ initial_config.py
@@ -0,0 +1,9 @@
+project_title = 'Graphic Novels for Ethics Teaching'
+project_description = 'how are graphic novels used in teaching?'
+inclusion_criteria = '- full text is available
+- full text mentions graphic novels or related concepts (see query); or
+- full text mentions ethics teaching or related concepts (see query); and 
+- full text is in English'
+criteria = ['Development process: Who created the GN, and was it designed for teaching?', 'Use process: Who uses it in teaching (authors or others)?', 'Format: Is the GN paper-based, online, or another medium?', 'Ethical domain: What area of ethics does it address?', 'Ethical issues: What specific issues are covered?', 'Target population: Who is the intended audience?', 'Pedagogical goals: Does it teach principles, critical thinking, or reasoning?', 'Teaching process: How is it used (lectures, group work, etc.)?', 'Interactivity: Are there guides, quizzes, or other materials?', 'Feedback: Does the teaching process gather and use feedback?', 'Evaluation: How is its impact or effectiveness measured?']
+firestore_collection = 'revmaster-gn-collection'
+firestore_collection_users = 'revmaster-gn-collection_users'
